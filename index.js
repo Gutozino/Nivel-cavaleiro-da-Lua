6fc123e528a6052f1b621cc013d63f553be5a428
Gutozino <171996753+Gutozino@users.noreply.github.com>
2025-08-11 19:58:34 -0300

    Terminado a parte back-end

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-console.log()
+let heroi = {
+    nome: "Cavaleiro da Lua",
+    xp: 10000
+};
+
+let nivel = "";
+
+if (heroi.xp <= 1000){
+    nivel = "Ferro";
+} else if (heroi.xp >= 1001 && heroi.xp <= 2000){
+    nivel = "Prata";
+} else if (heroi.xp >= 2001 && heroi.xp <= 5000){
+    nivel = "Ouro";
+} else if (heroi.xp >= 5001 && heroi.xp <= 7000){
+    nivel = "Platina";
+} else if (heroi.xp >= 8001 && heroi.xp <= 9000){
+    nivel = "Ascendente";
+} else if (heroi.xp >= 9001 && heroi.xp <= 10000){
+    nivel = "Imortal";
+} else if (heroi.xp > 10001){
+    nivel = "Radiante";
+}
+
+console.log("O Herói de nome " + heroi.nome + " está no nível de " + nivel);
